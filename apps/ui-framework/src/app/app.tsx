f833f87ce37ede5a7c63a03c2e041b3fb7bac618
Razec Dest2 <flesh_Cez@yahoo.com>
2022-11-14 21:10:31 +0200

    Meh

--- apps/ui-framework/src/app/app.tsx
+++ apps/ui-framework/src/app/app.tsx
@@ -5,7 +5,13 @@ import { ISideNavItem, UiHeader, UiSideNav } from '@ui-solution/ui-library';
 
 import { Route, Routes, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { API_URL, ApiResponse, IApp, IScenario } from '@ui-solution/ui-framework-api-interface';
+import {
+  API_URL,
+  ApiResponse,
+  IApp,
+  IScenario,
+  IHeader,
+} from '@ui-solution/ui-framework-api-interface';
 import Box from '@mui/material/Box';
 import { response } from 'express';
 
@@ -16,47 +22,51 @@ const {
   'grid-content': gridContentClass,
 } = styles;
 
-export function App() {
-  const [apiResponse, setApiResponse] = useState<IApp>();
+function useApp() {
   const [sideNavItems, setSideNavItems] = useState<ISideNavItem[]>();
+  const [header, setHeader] = useState<IHeader>();
 
   useEffect(() => {
     fetch(API_URL)
       .then((r) => r.json())
-      .then(result => {
+      .then((result) => {
         const res = result as IApp;
-        setApiResponse(res);
         // setScenarios(res.configuration.scenarios);
         const scenarios: IScenario[] = res.configuration.scenarios;
-        setSideNavItems(res.configuration.scenarios.map(sc => ({icon: sc.icon, label: sc.label}) as ISideNavItem ));
+        setHeader(res.configuration.header);
+        setSideNavItems(
+          scenarios.map(
+            (sc) => ({ icon: sc.icon, label: sc.label } as ISideNavItem)
+          )
+        );
       });
   }, []);
 
   const [isExpanded, setExpanded] = useState(false);
+  return { isExpanded, setExpanded, sideNavItems, header };
+}
+
+export function App() {
+  const { isExpanded, setExpanded, sideNavItems, header } = useApp();
   return (
-    <>
-      <div className={gridContainerClass}>
-        <div className={gridHeaderClass}>
-          <UiHeader onToggle={setExpanded} hasToggle title={apiResponse?.configuration?.header?.title}/>
-        </div>
-        <div className={gridSideNavClass}>
-          <UiSideNav open={isExpanded} items={sideNavItems} />
-        </div>
-        <div className={gridContentClass}>
-          <Box component="main" sx={{ width: "100%", height: "100%" }}>
-            <div> asd </div>
-            <div> asd </div>
-          </Box>
-        </div>
+    <div className={gridContainerClass}>
+      <div className={gridHeaderClass}>
+        <UiHeader
+          onToggle={setExpanded}
+          hasToggle
+          title={header?.title}
+        />
       </div>
-      {/* <UiHeader onToggle={setExpanded} hasToggle />
-      <Box sx={{ display: 'flex', width: '100%'}}>
-        <UiSideNav open={isExpanded} />
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <span>{apiResponse.message}</span>
+      <div className={gridSideNavClass}>
+        <UiSideNav open={isExpanded} items={sideNavItems} />
+      </div>
+      <div className={gridContentClass}>
+        <Box component="main" sx={{ width: '100%', height: '100%' }}>
+          <div> asd </div>
+          <div> asd </div>
         </Box>
-      </Box> */}
-    </>
+      </div>
+    </div>
   );
 }
 
